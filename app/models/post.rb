17b17c387963eec2dbb5c9be3c275d2a8468936c
Renato Rosa Franco <Renato_ny@live.com>
2019-04-25 16:50:03 -0300

    Post validations added

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,48 @@ class Post < ApplicationRecord
 
   belongs_to :category
   belongs_to :user
+
+  validates  :thumbnail,
+             presence: true,
+             uniqueness: false,
+             allow_blank: false,
+             length: { minimum: 3, maximum: 245 }
+    
+  validates  :title,
+             presence: true,
+             uniqueness: false,
+             allow_blank: false,
+             length: { minimum: 3, maximum: 50 }
+
+  validates  :description,
+             presence: true,
+             uniqueness: false,
+             allow_blank: false,
+             length: { minimum: 3, maximum: 245 }
+  
+  validates  :content,
+             presence: true,
+             uniqueness: false,
+             allow_blank: false,
+             length: { minimum: 3, maximum: 10_000 }
+  
+  validates  :published,
+             presence: false,
+             allow_blank: true
+
+  validates  :tags,
+             presence: true,
+             uniqueness: false,
+             allow_blank: false,
+             length: { minimum: 3, maximum: 145 }
+  
+  validates  :category_id,
+             presence: true,
+             uniqueness: false,
+             allow_blank: false
+  
+  validates  :user_id,
+             presence: true,
+             uniqueness: false,
+             allow_blank: false
 end
