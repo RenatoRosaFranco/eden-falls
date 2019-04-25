0ac2152f28c3ab3f423315d3d859191e921785c6
Renato Rosa Franco <Renato_ny@live.com>
2019-04-25 16:55:49 -0300

    API project configuration and routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,19 @@
 # frozen_string_literal: true
 Rails.application.routes.draw do
+  
+  # Dashboard
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  
+  # Authentication
   devise_for :users
 
-  
+  # API
+  namespace :api, constraint: { subdomain: 'api' } do
+    namespace :v1 do
+      # some json response here
+    end
+    namespace :v2 do
+      # some xml response here
+    end
+  end
 end
