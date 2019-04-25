410fed14569fc0868931a5f3e5f62a004ad1f217
Renato Rosa Franco <Renato_ny@live.com>
2019-04-25 16:41:20 -0300

    Tour validations added

--- app/models/tour.rb
+++ app/models/tour.rb
@@ -4,4 +4,44 @@ class Tour < ApplicationRecord
   self.primary_key = 'id'
 
   belongs_to :user
+
+  validates  :date,
+             presence: true,
+             uniqueness: false,
+             allow_blank: false
+  
+  validates  :location,
+             presence: true,
+             uniqueness: false,
+             allow_blank: false,
+             length: { minimum: 3, maximum: 145 }
+  
+  validates  :city,
+             presence: true,
+             uniqueness: false,
+             allow_blank: false,
+             length: { minimum: 3, maximum: 50 }
+  
+  validates  :state,
+             presence: true,
+             uniqueness: false,
+             allow_blank: false,
+             length: { minimum: 3, maximum: 30 }
+  
+  validates  :buy_ticket_link,
+             presence: true,
+             uniqueness: false,
+             allow_blank: false,
+             length: { minimum: 3, maximum: 245 }
+  
+  validates  :buy_vip_ticket_link,
+             presence: true,
+             uniqueness: false,
+             allow_blank: false,
+             length: { minimum: 3, maximum: 245 }
+  
+  validates  :user_id,
+             presence: true,
+             uniquness: false,
+             allow_blank: false
 end
