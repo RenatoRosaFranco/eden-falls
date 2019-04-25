7d97cf6e901b1839c5c231c78b44a8a38b99d2f5
Renato Rosa Franco <Renato_ny@live.com>
2019-04-25 16:19:35 -0300

    Category model validations added

--- app/models/category.rb
+++ app/models/category.rb
@@ -6,4 +6,27 @@ class Category < ApplicationRecord
   has_many :posts, dependent: :nullify
   belongs_to :category
   belongs_to :user
+
+  validates :name,
+            presence: true,
+            uniqueness: true,
+            allow_blank: false,
+            length: { minimum: 3, maximum: 50 }
+
+validates   :description,
+            presence: true,
+            uniqueness: false,
+            allow_blank: false,
+            length: { minimum: 3, maximum: 145 }
+
+validates   :tags,
+            presence: true,
+            uniqueness: false,
+            allow_blank: false,
+            length: { minimum: 3, maximum: 150 }
+
+validates   :user_id,
+            presence: true,
+            uniqueness: false,
+            allow_blank: false
 end
