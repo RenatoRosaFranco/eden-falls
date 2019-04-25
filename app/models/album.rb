0872ba379118f2cf0e25410b07f2a69405818a25
Renato Rosa Franco <Renato_ny@live.com>
2019-04-25 16:25:45 -0300

    Album validations added

--- app/models/album.rb
+++ app/models/album.rb
@@ -5,4 +5,39 @@ class Album < ApplicationRecord
 
   has_many :musics, dependent: :destroy
   belongs_to :user
+
+  validates :thumbnail,
+            presence: true,
+            uniqueness: false,
+            allow_blank: false,
+            length: { minimum: 3, maximum: 245 }
+  
+  validates :name,
+            presence: true,
+            uniqueness: false,
+            allow_blank: false,
+            length: { minimum: 3, maximum: 50 }
+  
+  validates :description,
+            presence: true,
+            uniqueness: false,
+            allow_blank: false,
+            length: { minimum: 3, maximum: 145 }
+  
+  validates :release_date,
+            presence: true,
+            uniqueness: false,
+            allow_blank: false,
+            length: { minimum: 3, maximum: 30}
+  
+  validates :tags,
+            presence: true,
+            uniqueness: false,
+            allow_blank: false,
+            length: { minimum: 3, maximum:  145 }
+  
+  validates :user_id,
+            presence: true,
+            uniqueness: false,
+            allow_blank: false
 end
