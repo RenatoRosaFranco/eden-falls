25a79415046929f76c6b5fde363463a1c19b5e6e
Renato Rosa Franco <Renato_ny@live.com>
2019-04-25 16:35:31 -0300

    Music validations added

--- app/models/music.rb
+++ app/models/music.rb
@@ -5,4 +5,33 @@ class Music < ApplicationRecord
 
   belongs_to :album, optional: true
   belongs_to :user
+
+  validates  :name,
+             presence: true,
+             uniqueness: false,
+             allow_blank: false,
+             length: { minimum: 3, maximum: 50 }
+
+  validates  :description,
+             presence: true,
+             uniqueness: false,
+             allow_blank: false,
+             length: { minimum: 3, maximum: 245 }
+  
+  validates  :letter,
+             presence: true,
+             uniqueness: false,
+             allow_blank: false,
+             length: { minimum: 3, maximum: 5_000 }
+  
+  validates  :tags,
+             presence: true,
+             uniqueness: false,
+             allow_blank: false,
+             length: { minimum: 3, maximum: 145 }
+  
+  validates  :user_id,
+             presence: true,
+             uniqueness: false,
+             allow_blank: false
 end
